8960a731729da25bbbc8d9eb7e5cae668bcb1470
HenryRivera <henry.rivera@nyu.edu>
2020-07-25 13:18:02 -0400

    Initial Commit

---
+++ tipMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipMe
+//
+//  Created by Henry Rivera on 7/25/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
